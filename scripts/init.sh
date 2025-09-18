f60f07eed0c60820d34741fb042ceb09450ada0f
Ali Khattab <aliktb@zohomail.eu>
2025-09-18 16:26:29 +0100

    Extend init script to get Kubeconfig automatically

--- scripts/init.sh
+++ scripts/init.sh
@@ -11,4 +11,12 @@ kubectl -n capmox-system rollout status deployment capmox-controller-manager --w
 kubectl -n capi-system rollout status deployment capi-controller-manager --watch --timeout=300s
 kubectl -n capi-ipam-in-cluster-system rollout status deployment capi-ipam-in-cluster-controller-manager --watch --timeout=300s
 
+# Deploy cluster
 kustomize build . | kubectl apply -f -
+
+# Wait for control plane to be ready
+echo "Waiting for control plane to be ready. This may take up to 10 mins..."
+kubectl wait taloscontrolplane.controlplane.cluster.x-k8s.io/aliktb-dev-talos-cp --for=condition=Ready=true --timeout=10m
+
+# Extract workload cluster Kubeconfig file
+bash scripts/get-kubeconfig.sh
